9885b16d2341302c04fa17265bc337140a46e25f
Humphrey Thuo <thuo@TMac09.local>
2016-05-10 12:26:34 +0300

    Comply with coding style guidelines
     - Change prototypes to ES6 classes
     - Use arrow functions
     - Use the `let` and `const` keywords instead of `var`
     - Use string templates instead of string concatenation
     - Keep all lines below 80 columns in length

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,91 +1,92 @@
 'use strict';
-describe('Inverted index', function() {
 
-  var index;
-  var filePath;
+describe('Inverted index', () => {
 
-  beforeEach(function() {
+  let index;
+  let filePath;
+
+  beforeEach(() => {
     index = new Index();
     filePath = 'books.json';
   });
 
-  describe('Read book data', function() {
+  describe('Read book data', () => {
 
-    var createIndex;
+    let createIndex;
 
-    beforeEach(function() {
-      createIndex = function() {
+    beforeEach(() => {
+      createIndex = () => {
         index.createIndex(filePath);
       };
     });
 
-    it('is able to open the file', function() {
+    it('is able to open the file', () => {
       expect(createIndex).not.toThrowError('Unable to open file');
     });
 
-    it('reads a non-empty file', function() {
+    it('reads a non-empty file', () => {
       expect(createIndex).not.toThrowError('The file is empty');
     });
 
-    it('reads a valid JSON file', function() {
+    it('reads a valid JSON file', () => {
       expect(createIndex).not.toThrowError('The file is not a valid JSON file');
     });
   });
 
-  describe('Populate data', function() {
+  describe('Populate data', () => {
 
-    beforeEach(function() {
+    beforeEach(() => {
       index.createIndex(filePath);
     });
 
-    it('creates the index', function() {
+    it('creates the index', () => {
       expect(index.getIndex()).not.toBeFalsy();
     });
 
-    it('maps the string keys to the correct objects', function() {
-      var allBooks = index.getBooks();
-      var indexMap = index.getIndex();
-      Object.keys(indexMap).forEach(function(term) {
-        indexMap[term].forEach(function(book) {
-          var bookContent = index.bookContent(allBooks[book]).toLowerCase();
+    it('maps the string keys to the correct objects', () => {
+      let allBooks = index.getBooks();
+      let indexMap = index.getIndex();
+      Object.keys(indexMap).forEach(term => {
+        indexMap[term].forEach(book => {
+          let bookContent = index.bookContent(allBooks[book]).toLowerCase();
           expect(bookContent).toMatch(term);
         });
       });
     });
   });
 
-  describe('Search index', function() {
+  describe('Search index', () => {
 
-    beforeEach(function() {
+    beforeEach(() => {
       index.createIndex(filePath);
     });
 
-    it('returns an array of the indices of the correct objects that contain the words in a search query', function() {
-      var results = index.searchIndex('alice wonderland');
+    it('returns an array of the indices of the correct objects that contain the words in a search query', () => {
+      let results = index.searchIndex('alice wonderland');
       expect(results).toEqual([0]);
 
       results = index.searchIndex('fellowship world');
       expect(results).toEqual([1, 0]);
     });
 
-    it('returns an array of the indices of the correct objects that contain the words in an array', function() {
-      var results = index.searchIndex(['alice', 'wonderland']);
+    it('returns an array of the indices of the correct objects that contain the words in an array', () => {
+      let results = index.searchIndex(['alice', 'wonderland']);
       expect(results).toEqual([0]);
 
       results = index.searchIndex(['fellowship', 'world']);
       expect(results).toEqual([1, 0]);
     });
 
-    it('returns an empty array when the search terms are not in the index', function() {
-      var results = index.searchIndex(['khaleesi', 'dragons']);
+    it('returns an empty array when the search terms are not in the index', () => {
+      let results = index.searchIndex(['khaleesi', 'dragons']);
       expect(results).toEqual([]);
 
       results = index.searchIndex('khaleesi dragons');
       expect(results).toEqual([]);
     });
 
-    it('returns an empty array when the search terms are empty', function() {
-      var results = index.searchIndex([]);
+    it('returns an empty array when the search terms are empty', () => {
+      let results = index.searchIndex([]);
       expect(results).toEqual([]);
 
       results = index.searchIndex('');
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,130 +1,128 @@
 'use strict';
 
 /* exported Index */
-/**
- * The inverted index class constructor.
- * @constructor
- */
-function Index() {
-  this.index = new Map();
-  this.books = null;
-  // Stop words obtained from:
-  // http://nlp.stanford.edu/IR-book/html/htmledition/dropping-common-terms-stop-words-1.html
-  this.stopWords = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
-    'he', 'in', 'into', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
-    'was', 'were', 'will', 'with'
-  ];
-}
+class Index {
 
-/**
- * Creates index from a file
- * @param {string} filePath - The location of the file
- */
-Index.prototype.createIndex = function(filePath) {
-  var _this = this;
-  _this.books = _this.loadJSON(filePath);
-  _this.books.forEach(function(book, index) {
-    var content = _this.bookContent(book);
-    var terms = _this.tokenize(content);
-    terms.forEach(function(term) {
-      term = term.toLowerCase();
-      // ignore stop words
-      if (_this.stopWords.indexOf(term) !== -1) {
-        return;
-      }
-      // if the term is not in the index add it to the index
-      if (!(term in _this.index)) {
-        // Use a set so that each book is only included once
-        _this.index[term] = new Set();
-      }
-      _this.index[term].add(index);
-    });
-  });
-};
-
-/**
- * Read a JSON file and parse it into a JavaScript object or array
- * @param {string} filePath - The location of the file
- */
-Index.prototype.loadJSON = function(filePath) {
-  var request = new XMLHttpRequest();
-  // `false` makes the request synchronous
-  request.open('GET', filePath, false);
-  request.send(null);
-  if (request.status === 200) {
-    if (request.responseText.trim().length === 0) {
-      throw Error('The file is empty');
-    }
-    try {
-      return JSON.parse(request.responseText);
-    } catch (e) {
-      throw Error('The file is not a valid JSON file');
-    }
+  constructor() {
+    this.index = new Map();
+    this.books = null;
+    // Stop words obtained from:
+    // http://nlp.stanford.edu/IR-book/html/htmledition/
+    // dropping-common-terms-stop-words-1.html
+    this.stopWords = [
+      'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
+      'he', 'in', 'into', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+      'was', 'were', 'will', 'with'
+    ];
   }
-  throw Error('Unable to open file');
-};
 
-/**
- * Returns a string that contains all the contents of a book i.e a combination
- * of the book title and the book text
- * @param {Object} book - The book
- * @returns {string} A string that contains all the contents of the book.
- */
-Index.prototype.bookContent = function(book) {
-  return book.title + ' ' + book.text;
-};
-
-/**
- * Breaks a string into constituents terms/words
- * @param {string} text - The string to tokenize
- * @returns {Array} An array of the terms.
- */
-Index.prototype.tokenize = function(text) {
-  // filter out punctuation characters
-  var punctuationRegex =
-    /[\!"#\$%&'\(\)\*\+\,\-\.\/:;<=>\?@\[\\\]\^_`\{\|\}~]/g;
-  text = text.replace(punctuationRegex, '');
-  // collapse whitespace
-  return text.replace(/\s+/g, ' ').split(' ');
-};
-
-/**
- * Returns the index as a Map object
- * @returns {Map} The index map
- */
-Index.prototype.getIndex = function() {
-  return this.index;
-};
-
-/**
- * Returns the books as an Array object
- * @returns {Array} The books array
- */
-Index.prototype.getBooks = function() {
-  return this.books;
-};
-
-/**
- * Search the index
- * @params {string|Array} query - What to search for
- */
-Index.prototype.searchIndex = function(query) {
-  var _this = this;
-  var terms;
-  if (typeof query === 'string') {
-    terms = _this.tokenize(query);
-  } else if (Array.isArray(query)) {
-    terms = query;
-  }
-  var results = new Set();
-  terms.forEach(function(term) {
-    term = term.toLowerCase();
-    if (term in _this.index) {
-      _this.index[term].forEach(function(book) {
-        results.add(book);
+  /**
+   * Creates index from a file
+   * @param {string} filePath - The location of the file
+   */
+  createIndex(filePath) {
+    this.books = this.loadJSON(filePath);
+    this.books.forEach((book, index) => {
+      let content = this.bookContent(book);
+      let terms = this.tokenize(content);
+      terms.forEach(term => {
+        term = term.toLowerCase();
+        // ignore stop words
+        if (this.stopWords.indexOf(term) !== -1) {
+          return;
+        }
+        // if the term is not in the index add it to the index
+        if (!(term in this.index)) {
+          // Use a set so that each book is only included once
+          this.index[term] = new Set();
+        }
+        this.index[term].add(index);
       });
+    });
+  }
+
+  /**
+   * Read a JSON file and parse it into a JavaScript object or array
+   * @param {string} filePath - The location of the file
+   */
+  loadJSON(filePath) {
+    let request = new XMLHttpRequest();
+    // `false` makes the request synchronous
+    request.open('GET', filePath, false);
+    request.send(null);
+    if (request.status === 200) {
+      if (request.responseText.trim().length === 0) {
+        throw Error('The file is empty');
+      }
+      try {
+        return JSON.parse(request.responseText);
+      } catch (e) {
+        throw Error('The file is not a valid JSON file');
+      }
     }
-  });
-  return Array.from(results);
-};
+    throw Error('Unable to open file');
+  }
+
+  /**
+   * Returns a string that contains all the contents of a book i.e a 
+   * combination of the book title and the book text
+   * @param {Object} book - The book
+   * @returns {string} A string that contains all the contents of the book.
+   */
+  bookContent(book) {
+    return `${book.title} ${book.text}`;
+  }
+
+  /**
+   * Breaks a string into constituents terms/words
+   * @param {string} text - The string to tokenize
+   * @returns {Array} An array of the terms.
+   */
+  tokenize(text) {
+    // filter out punctuation characters
+    let punctuationRegex =
+      /[\!"#\$%&'\(\)\*\+\,\-\.\/:;<=>\?@\[\\\]\^_`\{\|\}~]/g;
+    text = text.replace(punctuationRegex, '');
+    // collapse whitespace
+    return text.replace(/\s+/g, ' ').split(' ');
+  }
+
+  /**
+   * Returns the index as a Map object
+   * @returns {Map} The index map
+   */
+  getIndex() {
+    return this.index;
+  }
+
+  /**
+   * Returns the books as an Array object
+   * @returns {Array} The books array
+   */
+  getBooks() {
+    return this.books;
+  }
+
+  /**
+   * Search the index
+   * @params {string|Array} query - What to search for
+   */
+  searchIndex(query) {
+    let terms;
+    if (typeof query === 'string') {
+      terms = this.tokenize(query);
+    } else if (Array.isArray(query)) {
+      terms = query;
+    }
+    let results = new Set();
+    terms.forEach(term => {
+      term = term.toLowerCase();
+      if (term in this.index) {
+        this.index[term].forEach(book => {
+          results.add(book);
+        });
+      }
+    });
+    return Array.from(results);
+  }
+}
